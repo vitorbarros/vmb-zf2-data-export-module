0adcc60d0756e72f43b9e624e5016f55e3dab019
Matias Iglesias <matiasiglesias@meridiem.com.ar>
2017-03-04 21:48:25 -0300

    Removed redundant code

--- config/module.config.php
+++ config/module.config.php
@@ -27,18 +27,4 @@ return array(
             }
         ),
     ),
-    'doctrine' => array(
-        'driver' => array(
-            __NAMESPACE__ . '_driver' => array(
-                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
-                'cache' => 'array',
-                'paths' => array(__DIR__ . '/../src/' . __NAMESPACE__ . '/Entity'),
-            ),
-            'orm_default' => array(
-                'drivers' => array(
-                    __NAMESPACE__ . '\Entity' => __NAMESPACE__ . '_driver',
-                ),
-            ),
-        ),
-    ),
 );
